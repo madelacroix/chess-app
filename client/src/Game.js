ee4c499fdd4ee8f4ec8a7d4dbadafb84bff3e279
Mads <madeldelacruz@Madss-Mac-mini.local>
2023-06-05 20:06:06 +1200

    added notes so that i can understand what's going on

--- client/src/Game.js
+++ client/src/Game.js
@@ -4,8 +4,12 @@ import { Chessboard } from "react-chessboard"
 import CustomDialog from "./components/CustomDialog"
 
 function Game({ players, room, orientation, cleanup }) {
-    const chess = useMemo(() => new Chess(), [])
+    const chess = useMemo(() => new Chess(), []) 
+    // first time using useMemo. It allows you to cache between renders. Here, chess is getting cached so that this instance doesn't it doesn't get created on every re-render. This instance is going to be used for move validation and generation.
+
     const [fen, setFen] = useState(chess.fen())
+    // FEN stands for Forsyth-Edwards Notation, which i've never heard of before. but basically i think it stores the current position of all the pieces on the board so that it can properly restart a game. not sure. will update when i'm sure.
+
     const [over, setOver] = useState("")
 
     function onDrop() { }
@@ -13,8 +17,10 @@ function Game({ players, room, orientation, cleanup }) {
     return (
         <>
             <div className="board">
-                <Chessboard position={fen} onPieceDrop={onDrop} />
+                {/* Chessboard will be coming from react-chessboard. fen is going to store position of pieces. onPieceDrop will be called every time a piece moves. */}
+                <Chessboard position={fen} onPieceDrop={onDrop} />                
             </div>
+            {/* This Dialog will be rendered when the state "over" is true. That state basically tells us when the game is over and it should tell us how the game ended-- checkmate, stalemate or draw. */}
             <CustomDialog
                 open={Boolean(over)}
                 title={over}
